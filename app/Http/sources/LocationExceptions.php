08c4ca32527a1f72e973a6ce7fd20c31aebb8d3a
SkvOl <77995388+SkvOl@users.noreply.github.com>
2025-01-03 21:49:05 +0300

    Update LocationExceptions.php

--- app/Http/sources/LocationExceptions.php
+++ app/Http/sources/LocationExceptions.php
@@ -19,26 +19,46 @@ class LocationExceptions{
         $this->url = $url;
     }
 
+    /**
+     * Задаём исключения для url. 
+     * @return void
+     */
     public function setUrlExceptions(array $urlExceptions): void
     {
         $this->urlExceptions = $urlExceptions;
     }
 
+    /**
+     * Задаём исключения для ip. 
+     * @return void
+     */
     public function setIpExceptions(array $ipExceptions): void
     {
         $this->ipExceptions = $ipExceptions;
     }
 
+    /**
+     * Проверяем, находится ли ip клиента в нашей локальной сети для персонализации response. 
+     * @return bool
+     */
     public function isLocal(): bool
     {
         return str_contains($this->ip, env('LOCAL_IP'));
     }
 
+    /**
+     * Проверяем, находится ли url клиента в списке исключений для персонализации response. 
+     * @return bool
+     */
     public function isUrlExceptions(): bool
     {
         return self::array_in_like($this->url, $this->urlExceptions) OR self::in_array_like($this->url, $this->urlExceptions);
     }
 
+    /**
+     * Проверяем, находится ли ip клиента в списке исключений для персонализации response. 
+     * @return bool
+     */
     public function isIpExceptions(): bool
     {
         return self::array_in_like($this->ip, $this->ipExceptions) OR self::in_array_like($this->ip, $this->ipExceptions);
